08165280b2582e263b909b41dc45a35abcf99df9
VuongDucHuy <vuongduchuy1610@gmail.com>
2023-10-23 13:48:41 +0700

    gdbackend-fontend

---
+++ footer.php
@@ -0,0 +1,19 @@
+<div class="jumbotron">
+     
+<div class="appeal">            
+        </div>
+ 
+
+<h2>Vuowng Duc Huy</h2>
+ 
+</div>
+ 
+</div>
+ 
+<!--/. Container-->
+</div>
+ 
+</div>
+ 
+</body>
+</html>
